45f1d96c36cfdf5c6a03012d549686e8a10eb0a7
savasolar <savasolar@gmail.com>
2026-01-31 15:30:48 -0500

    random failure

--- Source/PluginProcessor.cpp
+++ Source/PluginProcessor.cpp
@@ -1,6 +1,15 @@
+/*
+  ==============================================================================
+
+    This file contains the basic framework code for a JUCE plugin processor.
+
+  ==============================================================================
+*/
+
 #include "PluginProcessor.h"
 #include "PluginEditor.h"
 
+//==============================================================================
 CounterTune_v2AudioProcessor::CounterTune_v2AudioProcessor()
 #ifndef JucePlugin_PreferredChannelConfigurations
      : AudioProcessor (BusesProperties()
@@ -13,14 +22,13 @@ CounterTune_v2AudioProcessor::CounterTune_v2AudioProcessor()
                        )
 #endif
 {
-//    dywapitch_inittracking(&pitchTracker);
 }
 
 CounterTune_v2AudioProcessor::~CounterTune_v2AudioProcessor()
 {
-
 }
 
+//==============================================================================
 const juce::String CounterTune_v2AudioProcessor::getName() const
 {
     return JucePlugin_Name;
@@ -60,7 +68,8 @@ double CounterTune_v2AudioProcessor::getTailLengthSeconds() const
 
 int CounterTune_v2AudioProcessor::getNumPrograms()
 {
-    return 1;
+    return 1;   // NB: some hosts don't cope very well if you tell them there are 0 programs,
+                // so this should be at least 1, even if you're not really implementing programs.
 }
 
 int CounterTune_v2AudioProcessor::getCurrentProgram()
@@ -79,19 +88,19 @@ const juce::String CounterTune_v2AudioProcessor::getProgramName (int index)
 
 void CounterTune_v2AudioProcessor::changeProgramName (int index, const juce::String& newName)
 {
-
 }
 
+//==============================================================================
 void CounterTune_v2AudioProcessor::prepareToPlay (double sampleRate, int samplesPerBlock)
 {
-//    analysisBuffer.setSize(1, 1024, true);
-//    pitchDetectorFillPos = 0;
-
+    // Use this method as the place to do any pre-playback
+    // initialisation that you need..
 }
 
 void CounterTune_v2AudioProcessor::releaseResources()
 {
-
+    // When playback stops, you can use this as an opportunity to free up any
+    // spare memory, etc.
 }
 
 #ifndef JucePlugin_PreferredChannelConfigurations
@@ -101,11 +110,15 @@ bool CounterTune_v2AudioProcessor::isBusesLayoutSupported (const BusesLayout& la
     juce::ignoreUnused (layouts);
     return true;
   #else
-
+    // This is the place where you check if the layout is supported.
+    // In this template code we only support mono or stereo.
+    // Some plugin hosts, such as certain GarageBand versions, will only
+    // load plugins that support stereo bus layouts.
     if (layouts.getMainOutputChannelSet() != juce::AudioChannelSet::mono()
      && layouts.getMainOutputChannelSet() != juce::AudioChannelSet::stereo())
         return false;
 
+    // This checks if the input layout matches the output layout
    #if ! JucePlugin_IsSynth
     if (layouts.getMainOutputChannelSet() != layouts.getMainInputChannelSet())
         return false;
@@ -119,87 +132,36 @@ bool CounterTune_v2AudioProcessor::isBusesLayoutSupported (const BusesLayout& la
 void CounterTune_v2AudioProcessor::processBlock (juce::AudioBuffer<float>& buffer, juce::MidiBuffer& midiMessages)
 {
     juce::ScopedNoDenormals noDenormals;
+    auto totalNumInputChannels  = getTotalNumInputChannels();
+    auto totalNumOutputChannels = getTotalNumOutputChannels();
 
-//    int numSamples = buffer.getNumSamples();
+    // In case we have more outputs than inputs, this code clears any output
+    // channels that didn't contain input data, (because these aren't
+    // guaranteed to be empty - they may contain garbage).
+    // This is here to avoid people getting screaming feedback
+    // when they first compile a plugin, but obviously you don't need to keep
+    // this code if your algorithm always overwrites all the output channels.
+    for (auto i = totalNumInputChannels; i < totalNumOutputChannels; ++i)
+        buffer.clear (i, 0, buffer.getNumSamples());
 
-    // synchronize daw bpm
-    // ...
-
-
-    // get pitch readouts
-
-    // Mix current block to mono for pitch detection
-    /*
-    juce::AudioBuffer<float> monoBlock(1, numSamples);
-    monoBlock.clear();
-    int numChannels = juce::jmin(getTotalNumInputChannels(), buffer.getNumChannels());
-    for (int ch = 0; ch < numChannels; ++ch)
+    // This is the place where you'd normally do the guts of your plugin's
+    // audio processing...
+    // Make sure to reset the state if your inner loop is processing
+    // the samples and the outer loop is handling the channels.
+    // Alternatively, you can process the samples with the channels
+    // interleaved by keeping the same state.
+    for (int channel = 0; channel < totalNumInputChannels; ++channel)
     {
-        monoBlock.addFrom(0, 0, buffer, ch, 0, numSamples);
+        auto* channelData = buffer.getWritePointer (channel);
+
+        // ..do something to the data...
     }
-    if (numChannels > 0) monoBlock.applyGain(1.0f / numChannels);
-    auto* monoData = monoBlock.getReadPointer(0);
-
-    // Accumulate for pitch detection
-    int analysisSpaceLeft = analysisBuffer.getNumSamples() - pitchDetectorFillPos;
-    int analysisToCopy = juce::jmin(analysisSpaceLeft, numSamples);
-    analysisBuffer.copyFrom(0, pitchDetectorFillPos, monoData, analysisToCopy);
-    pitchDetectorFillPos += analysisToCopy;
-
-    // If full, detect pitch and store MIDI note
-    if (pitchDetectorFillPos >= analysisBuffer.getNumSamples())
-    {
-        // DYWAPitchTrack uses double*, but analysisBuffer is float*. Convert temporarily.
-        std::vector<double> doubleSamples(1024);
-        for (int i = 0; i < 1024; ++i)
-            doubleSamples[i] = analysisBuffer.getSample(0, i);
-
-        // Compute pitch (returns Hz, or 0.0 if no pitch detected).
-        double pitch = dywapitch_computepitch(&pitchTracker, doubleSamples.data(), 0, 1024);
-        pitch *= (getSampleRate() / 44100.0); // Scale for DYWAPitchTrack's 44100 assumption
-
-        DBG(pitch);
-
-//        int midiNote = frequencyToMidiNote(static_cast<float>(pitch));
-
-//        detectedNoteNumbers.push_back(midiNote);
-
-        pitchDetectorFillPos = 0;
-    }
-
-    // Handle overflow
-    if (analysisToCopy < numSamples)
-    {
-        analysisBuffer.copyFrom(0, 0, monoData + analysisToCopy, numSamples - analysisToCopy);
-        pitchDetectorFillPos = numSamples - analysisToCopy;
-    }
-    */
-
-
-    // detect if there is "musical" input audio
-        // based on pitch detector readout
-
-    // record input audio buffer
-
-    // record pitches to 32 midi note numbers
-        // once full, call generateMelody and generate synthetic voice
-
-    // generate synthetic voice
-        // isolate best note
-            // preprocess, trim
-            // use custom overlap-add method to elongate
-            // post-process, add fade-in and fade-out ramps (spread across 500 samples each, ~15ms)
-
-    // synthesize generated sequences
-        // stop/start and apply pitch stretching to synthetic voice buffer
-        // add post-processing like compression
-
-
 }
 
+//==============================================================================
 bool CounterTune_v2AudioProcessor::hasEditor() const
 {
-    return true;
+    return true; // (change this to false if you choose to not supply an editor)
 }
 
 juce::AudioProcessorEditor* CounterTune_v2AudioProcessor::createEditor()
@@ -207,6 +169,7 @@ juce::AudioProcessorEditor* CounterTune_v2AudioProcessor::createEditor()
     return new CounterTune_v2AudioProcessorEditor (*this);
 }
 
+//==============================================================================
 void CounterTune_v2AudioProcessor::getStateInformation (juce::MemoryBlock& destData)
 {
     // You should use this method to store your parameters in the memory block.
@@ -220,6 +183,8 @@ void CounterTune_v2AudioProcessor::setStateInformation (const void* data, int si
     // whose contents will have been created by the getStateInformation() call.
 }
 
+//==============================================================================
+// This creates new instances of the plugin..
 juce::AudioProcessor* JUCE_CALLTYPE createPluginFilter()
 {
     return new CounterTune_v2AudioProcessor();
